f81a1b39dc60b4343aea98440920460440170229
fun <apaulinb7@gmail.com>
2020-03-22 22:48:51 -0400

    Modified Board.cs to have a simpler singleton implementation. Barebones GameManager.cs

--- CounterPlay/Board.cs
+++ CounterPlay/Board.cs
@@ -5,23 +5,30 @@ namespace CounterPlay
 {
 	public sealed class Board
 	{
-		private static readonly Board instance = new Board();
+		private static Board instance;
 		
 		public Player[] Players;
 		public Deck Deck;
 		public ScrapPile ScrapPile;
 		public Card.CardList CardsInPlay;
-		
-		private Board() { }
-		public void Initialize(int AmountOfPlayers, Card[] StartingDeck)
+
+		private Board(int AmountOfPlayers, Card[] StartingDeck)
 		{
-			if (instance != null)
-				throw new Exception("AlreadyInitializedException");
-			
 			Players = new Player[AmountOfPlayers];
 			Deck = new Deck(StartingDeck);
 			ScrapPile = new ScrapPile();
-			CardsInPlay.Cards = new List<Card>();
+			CardsInPlay = new Card.CardList();
+		}
+		public static Board Initialize(int AmountOfPlayers, Card[] StartingDeck)
+		{
+			if (instance != null)
+			{
+				throw new Exception("Already initialized.");
+			}
+			
+			instance = new Board(AmountOfPlayers, StartingDeck);
+
+			return instance;
 		}
 
 		public static Board Instance => instance;
--- CounterPlay/GameManager.cs
+++ CounterPlay/GameManager.cs
@@ -1,7 +1,21 @@
+using System;
+
 namespace CounterPlay
 {
 	public class GameManager
 	{
-		
+		public int NumberOfPlayers = 2;
+
+		public Board MainBoard;
+
+		public GameManager(Card[] allTheCards)
+		{
+			Board.Initialize(2, allTheCards);
+		}
+
+		public override String ToString()
+		{
+			return "This is the GM speaking... we have " + NumberOfPlayers + " players.";
+		}
 	}
 }
