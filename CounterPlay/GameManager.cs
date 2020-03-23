b06088602299a87e891ef6ebb795c95a92959669
fun <apaulinb7@gmail.com>
2020-03-22 23:17:18 -0400

    GameManager: Added beginning of GameState Logic.

--- CounterPlay/GameManager.cs
+++ CounterPlay/GameManager.cs
@@ -2,6 +2,16 @@ using System;
 
 namespace CounterPlay
 {
+	//TODO:
+	// Make a GameState Class, that keeps track of the turn state,
+	// Current Player, and Logs all the actions so that the Game
+	// Can be replayed
+	public enum GameState {
+		BOARD_BOT, PLAYER_BOT, PLAYER_DRAW,
+		PLAYER_TURN, PLAYER_EOT, PLAYER_DISCARD,
+		PLAYER_COUNTER_RESET, BOARD_EOT, HANDOFF
+	}
+
 	public class GameManager
 	{
 		public int NumberOfPlayers = 2;
@@ -10,7 +20,9 @@ namespace CounterPlay
 
 		public GameManager(Card[] allTheCards)
 		{
-			Board.Initialize(2, allTheCards);
+			MainBoard = Board.Initialize(2, allTheCards);
+			
+			Console.WriteLine(MainBoard);
 		}
 
 		public override String ToString()
